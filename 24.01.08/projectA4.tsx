e1378af9009dda2efc89e54574bf5e52b972a170
ChaeYi <chaeyi990731@gmail.com>
2024-01-09 18:31:28 +0900

    response.ok란 무엇인지..

--- 24.01.08/projectA4.tsx
+++ 24.01.08/projectA4.tsx
@@ -24,3 +24,14 @@ useEffect(() => {
 /*
 handLeSubscribe() 메서드는 fetch() 메서드를 사용하여 서버에 구독 요청을 보낸다.
 '.subscribe' 로 작성된 경로는, 임의의 GET 요청을 받아 처리하는 라우터를 의미하고, 다른 REST APi 처럼 주소의 형태, 경로의 형태 등 여러가지가 될 수 있다 * */
+
+const handleSubscribe = async () => {
+    try {
+        const response = await fetch('/subscribe');
+        if (!response.ok){
+            /*
+            response.ok 는 fetch() 메서드가 정상적으로 동작했는지를 확인하는 속성 */
+            
+        }
+    }
+}
